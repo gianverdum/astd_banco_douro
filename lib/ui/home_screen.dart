64172c9f9092949d11c1d771c0a470c342b88738
Giancarlo Verdum <gian.verdum@gmail.com>
2025-12-15 14:41:30 -0300

    Login and home screens and backend merge

---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_banco_douro/models/account.dart';
+import 'package:flutter_banco_douro/ui/styles/colors.dart';
+import 'package:flutter_banco_douro/ui/widgets/account_widget.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColor.lightGrey,
+        title: const Text("Sistema de Gestão de Contas"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, "login");
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: AccountWidget(
+          account: Account(
+            id: "ID001",
+            name: "Daniel",
+            lastName: "Silveira",
+            balance: 350,
+            accountType: null,
+          ),
+        ),
+      ),
+    );
+  }
+}
